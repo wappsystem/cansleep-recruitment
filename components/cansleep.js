fa46994e3f3ef97dbd60335b5265d351f9a3fcb9
Gunnar Unger <gunnar.unger@gmail.com>
2019-06-26 16:05:38 +1000

    update

---
+++ components/cansleep.js
@@ -0,0 +1,6 @@
+(function(){
+    var modules={
+        "panel-main-cansleep":    			 {url:"$A/modules/elig-questions-data.html",Table:"dev-cansleep-recruitment-record"},
+   }
+    for(m in modules){$vm.module_list[m]=modules[m];}
+})();
