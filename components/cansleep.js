46aaaeaa9615e54ce19ff0e815a84b67f7997a9b
Gunnar Unger <gunnar.unger@gmail.com>
2019-07-25 15:12:50 +1000

    update

--- components/cansleep.js
+++ components/cansleep.js
@@ -1,8 +1,8 @@
 (function(){
     var modules={
-        "panel-main-cansleep":    			 {url:"$A/modules/elig-questions-data.html",Table:"dev-cansleep-recruitment-record"},
-        "panel-main-cansleep-eligible":      {url:"$A/modules/elig-questions-eligible-data.html",Table:"dev-cansleep-recruitment-record"},
-        "panel-main-cansleep-non-eligible":  {url:"$A/modules/elig-questions-non-eligible-data.html",Table:"dev-cansleep-recruitment-record"},
+        "panel-main-cansleep":    			 {url:"$A/modules/elig-questions-data.html",Table:"cansleep-recruitment-record"},
+        "panel-main-cansleep-eligible":      {url:"$A/modules/elig-questions-eligible-data.html",Table:"cansleep-recruitment-record"},
+        "panel-main-cansleep-non-eligible":  {url:"$A/modules/elig-questions-non-eligible-data.html",Table:"cansleep-recruitment-record"},
    }
     for(m in modules){$vm.module_list[m]=modules[m];}
 })();
